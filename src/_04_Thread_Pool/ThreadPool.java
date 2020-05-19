43a61c8670c17ca52127f9af06c2a3df46415b5c
RyanTrubey <ryan92504@gmail.com>
2020-05-19 16:35:09 -0700

    Finished ThreadPool and SynchronizedSwimmers

--- src/_04_Thread_Pool/ThreadPool.java
+++ src/_04_Thread_Pool/ThreadPool.java
@@ -5,12 +5,28 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 public class ThreadPool {
 	Thread[] threads;
 	ConcurrentLinkedQueue<Task> taskQueue;
-	
+
 	public ThreadPool(int totalThreads) {
 		threads = new Thread[totalThreads];
-		for(int i = 0; i < totalThreads; i++) {
-			threads[i] = new Thread(() -> new Worker());
-		}
 		taskQueue = new ConcurrentLinkedQueue<Task>();
+		for (int i = 0; i < totalThreads; i++) {
+			threads[i] = new Thread(new Worker(taskQueue));
+		}
+	}
+
+	public void addTask(Task task) {
+		taskQueue.add(task);
+	}
+
+	public void start() {
+		for (int i = 0; i < threads.length; i++) {
+			threads[i].start();
+			try {
+				threads[i].join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 }
